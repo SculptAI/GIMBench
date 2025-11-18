66f17993439339eb5e2f4abd835268d71624496a
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-11-18 14:02:12 +0800

    chore: migrate eval commands

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,3 +1,7 @@
+# TODO: OUTDATED
+
+# ─── COPIED FROM GIM-research/eval/commands.sh ────────────────────────────────
+
 # DO NOT RUN THIS SCRIPT DIRECTLY.
 # Commands below are intended to be copy-pasted into a terminal.
 
@@ -44,3 +48,21 @@ for BUDGET in {2..18..2}; do python -m eval.mcqa.medmcqa --model_name artifacts/
 for BUDGET in {2..18..2}; do python -m eval.mcqa.mmlu_pro --model_name artifacts/2510233-mixture-low/sft-gim --is_gim --reason_budget "$BUDGET" --num_proc 40 --first_n 1000; done
 for BUDGET in {2..18..2}; do python -m eval.mcqa.qasc --model_name artifacts/2510233-mixture-low/sft-gim --is_gim --reason_budget "$BUDGET" --num_proc 40 --first_n 920; done
 sudo shutdown -h +3
+
+
+# ─── COPIED FROM GIMKit/evals/commands.sh ─────────────────────────────────────
+
+# Run some exports before running the commands
+# export OPENAI_API_KEY="xxx"
+# export OPENAI_API_BASE="https://xxx/v1"
+# export HF_TOKEN="xxx"
+
+python -m evals.run --model_type openai --api_key $OPENAI_API_KEY --api_base $OPENAI_API_BASE --model_name openai/gpt-5
+python -m evals.run --model_type openai --api_key $OPENAI_API_KEY --api_base $OPENAI_API_BASE --model_name openai/gpt-5 --use_gim_prompt
+python -m evals.run --model_type openai --api_key $OPENAI_API_KEY --api_base $OPENAI_API_BASE --model_name openai/gpt-5 --use_gim_prompt --output_type json
+
+python -m evals.run --model_type vllm --model_name Qwen/Qwen3-0.6B --use_gim_prompt --output_type cfg
+
+python -m evals.run --model_type vllm --model_name Sculpt-AI/GIM-test
+python -m evals.run --model_type vllm --model_name Sculpt-AI/GIM-test --output_type json
+python -m evals.run --model_type vllm --model_name Sculpt-AI/GIM-test --output_type cfg
