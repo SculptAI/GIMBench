abc0c2821004071b301743fbb332e723c8c9198d
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2026-01-25 16:20:50 +0800

    refactor: update evaluation script to use new module structure

--- results/260124-kdd-expt-1-rerun/*eval.sh
+++ results/260124-kdd-expt-1-rerun/*eval.sh
@@ -25,28 +25,28 @@ hf download Sculpt-AI/2601198-grid-lr2e-6-ga16
 hf download Sculpt-AI/2601199-grid-lr2e-6-ga24
 
 
-python -m gimbench.ctp.gim_sft --model_type openai --model_name openai/gpt-5.2 --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100 --base_model_vocab_size 201088
-python -m gimbench.ctp.gim_sft --model_type openai --model_name google/gemini-3-flash-preview --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100 --base_model_vocab_size 256128
-python -m gimbench.ctp.gim_sft --model_type openai --model_name moonshotai/kimi-k2-thinking --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100 --base_model_vocab_size 163840
-python -m gimbench.ctp.gim_sft --model_type openai --model_name deepseek/deepseek-chat-v3-0324 --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100 --base_model_vocab_size 129280
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Qwen/Qwen3-4B-Instruct-2507 --use_gim_prompt --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Qwen/Qwen3-1.7B --use_gim_prompt --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/09251-gim-sft-tmp --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2510231-mixture-high --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2510232-mixture-mid --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601081-mix1 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601091-mix2 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601092-mix3 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601093-mix4 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601191-grid-lr2e-5-ga12 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601192-grid-lr2e-5-ga16 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601193-grid-lr2e-5-ga24 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601194-grid-lr2e-4-ga12 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601195-grid-lr2e-4-ga16 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601196-grid-lr2e-4-ga24 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601197-grid-lr2e-6-ga12 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601198-grid-lr2e-6-ga16 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
-python -m gimbench.ctp.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601199-grid-lr2e-6-ga24 --output_type cfg --ref_model_device cpu --first_n 100 --base_model_vocab_size 151936
+python -m gimbench.ppl.gim_sft --model_type openai --model_name openai/gpt-5.2 --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type openai --model_name google/gemini-3-flash-preview --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type openai --model_name moonshotai/kimi-k2-thinking --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type openai --model_name deepseek/deepseek-chat-v3-0324 --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Qwen/Qwen3-4B-Instruct-2507 --use_gim_prompt --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Qwen/Qwen3-1.7B --use_gim_prompt --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/09251-gim-sft-tmp --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2510231-mixture-high --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2510232-mixture-mid --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601081-mix1 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601091-mix2 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601092-mix3 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601093-mix4 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601191-grid-lr2e-5-ga12 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601192-grid-lr2e-5-ga16 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601193-grid-lr2e-5-ga24 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601194-grid-lr2e-4-ga12 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601195-grid-lr2e-4-ga16 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601196-grid-lr2e-4-ga24 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601197-grid-lr2e-6-ga12 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601198-grid-lr2e-6-ga16 --output_type cfg --ref_model_device cpu --first_n 100
+python -m gimbench.ppl.gim_sft --model_type vllm-offline --model_name Sculpt-AI/2601199-grid-lr2e-6-ga24 --output_type cfg --ref_model_device cpu --first_n 100
 
 
 python -m gimbench.match.gim_regex --model_type openai --model_name openai/gpt-5.2 --api_key $API_KEY --base_url $API_BASE --use_gim_prompt --output_type json --first_n 100
