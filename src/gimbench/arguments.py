72111e491ff69dc6ff09d217e2188354b07ea8ff
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2026-01-25 16:08:53 +0800

    refactor: replace CTP with windowed normalized perplexity metrics (#52)
    * chore: cp src/gimbench/ctp src/gimbench/ppl
    
    * refactor: remove src/gimbench/ctp
    
    * refactor: rename CTP evaluator to PPL and update related arguments and calculations
    
    * fix: ref_tokenizer.vocab_size
    
    * refactor: update comments and variable names for clarity in PPL evaluation metrics
    
    * refactor: improve type annotations and variable names in PPLEvaluator
    
    * refactor: resturcture the code
    
    * refactor: update argument choices for PPL evaluation and enhance type annotations in EvalItemResult

--- src/gimbench/arguments.py
+++ src/gimbench/arguments.py
@@ -84,12 +84,12 @@ def _add_evaluator_args(parser):
     )
 
 
-def _add_ctp_eval_args(parser):
+def _add_ppl_eval_args(parser):
     parser.add_argument(
         "--ref_model_name",
         type=str,
         default="google/gemma-3-270m",
-        help="Reference model for Composite Text Perplexity (CTP) evaluation",
+        help="Reference model for Perplexity (PPL) evaluation",
     )
     parser.add_argument(
         "--ref_model_device",
@@ -98,16 +98,16 @@ def _add_ctp_eval_args(parser):
         help="Device for the reference model",
     )
     parser.add_argument(
-        "--base_model_vocab_size",
-        type=int,
-        default=0,
-        help="Vocabulary size of the base model for Normalized CTP calculation",
-    )
-    parser.add_argument(
-        "--ctp_alpha",
+        "--norm_ppl_alpha",
         type=float,
         default=0.2,
-        help="Scaling factor alpha for Normalized CTP",
+        help="Scaling factor alpha for Normalized PPL",
+    )
+    parser.add_argument(
+        "--ppl_window_k",
+        type=int,
+        default=16,
+        help="Window size for PPL calculation. The window will be of size 2k",
     )
 
 
@@ -143,7 +143,7 @@ def get_args() -> argparse.Namespace:
     _add_model_args(parser)
     _add_sample_args(parser)
     _add_evaluator_args(parser)
-    _add_ctp_eval_args(parser)
+    _add_ppl_eval_args(parser)
     _add_mcqa_eval_args(parser)
     args = parser.parse_args()
     validate_and_standardize(args)
