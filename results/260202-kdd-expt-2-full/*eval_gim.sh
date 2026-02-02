74777578a0f29c1ffbe9cd8a628f808b0e1f4660
Duguce <92162236+Duguce@users.noreply.github.com>
2026-02-02 16:46:11 +0800

    chore: remove gim prompt about gim models

--- results/260202-kdd-expt-2-full/*eval_gim.sh
+++ results/260202-kdd-expt-2-full/*eval_gim.sh
@@ -2,11 +2,6 @@
 set -x
 
 
-GIM_MODELS=(
-    "Sculpt-AI/2601261-qwen-4b"
-    "Sculpt-AI/2601261-qwen-4b-50k"
-)
-
 setup_prompt() {
     script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     if [ -f "$script_dir/auto_budget_prompt.txt" ]; then
@@ -29,10 +24,10 @@ run_gim_experiments() {
     for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.medmcqa" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Qwen/Qwen3-4B-Instruct-2507" \
         --reason_budget "$BUDGET" --num_proc 40 --first_n 500; done
 
-    python -m "gimbench.mcqa.medmcqa" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
+    python -m "gimbench.mcqa.medmcqa" --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
         --auto_budget --auto_budget_prompt "$AUTO_BUDGET_PROMPT" \
         --reason_step_desc "$REASON_STEP_DESC" --num_proc 40 --first_n 500
-    for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.medmcqa" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
+    for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.medmcqa" --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
         --reason_budget "$BUDGET" --num_proc 40 --first_n 500; done
 
     python -m "gimbench.mcqa.qasc" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Qwen/Qwen3-4B-Instruct-2507" \
@@ -41,10 +36,10 @@ run_gim_experiments() {
     for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.qasc" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Qwen/Qwen3-4B-Instruct-2507" \
         --reason_budget "$BUDGET" --num_proc 40 --first_n -1; done
 
-    python -m "gimbench.mcqa.qasc" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
+    python -m "gimbench.mcqa.qasc" --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
         --auto_budget --auto_budget_prompt "$AUTO_BUDGET_PROMPT" \
         --reason_step_desc "$REASON_STEP_DESC" --num_proc 40 --first_n -1
-    for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.qasc" --use_gim_prompt --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
+    for BUDGET in {1..7..2}; do python -m "gimbench.mcqa.qasc" --output_type cfg --model_type vllm-offline --model_name "Sculpt-AI/2601261-qwen-4b-50k" \
         --reason_budget "$BUDGET" --num_proc 40 --first_n -1; done
 
 }
