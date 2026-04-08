65b51839e74d3e142de5ff9b31596a46d095847c
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2026-04-08 21:14:36 +0800

    update eval results

--- results/260407-humaneval-bench/*eval.sh
+++ results/260407-humaneval-bench/*eval.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Qwen/Qwen3-1.7B --no_gimkit --first_n 100
-python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Sculpt-AI/GIM-1.7B --output_type cfg --first_n 100
+# python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Sculpt-AI/GIM-4B --output_type cfg --first_n 200
+# python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Qwen/Qwen3-4B-Instruct-2507 --no_gimkit --first_n 200
+# python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Sculpt-AI/GIM-1.7B --output_type cfg --first_n 200
+python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Qwen/Qwen3-1.7B --no_gimkit --first_n 200
 
-python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Qwen/Qwen3-4B-Instruct-2507 --no_gimkit --first_n 100
-python -m gimbench.code.humaneval_infilling --model_type vllm-offline --model_name Sculpt-AI/GIM-4B --output_type cfg --first_n 100
-
-shutdown -h +3
+# shutdown -h +3
